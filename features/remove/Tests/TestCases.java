880ca58bb3d9d6f187ff34223acfb0af6f66c83f
MarkMGray <markmgray2010@gmail.com>
2014-10-12 20:55:45 -0500

    Updated test case for delete

--- features/remove/Tests/TestCases.java
+++ features/remove/Tests/TestCases.java
@@ -1,6 +1,9 @@
 package Tests;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 
 import org.junit.Test;
@@ -8,11 +11,11 @@ import org.junit.Test;
 import FList.MyList;
 import LApp.Entity;
 import LApp.Main;
-
+import java.util.*;
 public class TestCases {
 	@Test
 	public void removeNodesTest() {
-		MyList myList = new MyList();
+		MyList myList = new MyList<Entity>();
 		Main.addArray(myList, Entity.entArray1);
 		Main.addArray(myList, Entity.entArray2);
 		Entity[] ent2 = Entity.entArray2;
@@ -59,6 +62,43 @@ public class TestCases {
 		} else {
 			fail("Test: Remove nodes from List - Failed");
 		}
+	}
+	
+	@Test
+	public void removeGenericNodeTest(){
+		MyList myList = new MyList<ArrayList<Integer> >();
+		
+		ArrayList[] test1 = {
+	        	new ArrayList<Integer>(new ArrayList<Integer>(Arrays.asList(1,2,3,4))),
+	        	new ArrayList<Integer>(new ArrayList<Integer>(Arrays.asList(90, 80, 70, 60))), 
+	        	new ArrayList<Integer>(new ArrayList<Integer>(Arrays.asList(3, 4, 5, 5))),
+	            new ArrayList<Integer>(new ArrayList<Integer>(Arrays.asList(7))), 
+	            new ArrayList<Integer>(new ArrayList<Integer>(Arrays.asList(22, 23))), 
+	            new ArrayList<Integer>(new ArrayList<Integer>(Arrays.asList(20,30,40,50,60,70,80) ))};
+	        
+		for(int j = 0; j < test1.length; j++) {
+    		myList.insert(test1[j]);
+    	}
+		
+		for(Iterator i = myList.iterator(); i.hasNext();){
+			ArrayList<Integer> obj = (ArrayList) i.next();
+			if(obj.get(0).intValue() % 2 == 0)
+				i.remove();
+		}
+		
+		boolean pass = true;
+		for(Iterator i = myList.iterator(); i.hasNext();){
+			ArrayList<Integer> obj = (ArrayList) i.next();
+			if(obj.get(0).intValue() % 2 == 0)
+				pass = false;
+		}
+		
+		if(pass){
+			System.out.println("Test: Remove even leading list items - Passed");
+		} else {
+			fail("Test: Remove even leading list items - Failed");
+		}
+		
 	}
 	
 	@Test
